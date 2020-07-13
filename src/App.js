cf3bb72700639f5a1b55489052831c215f124f87
jec <jec.valentin@stillco.ro>
2020-07-13 18:04:43 +0300

    adad

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 import "./App.css";
 import firebase from "./firebase";
 import { SpellInput } from "./SpellInput";
+import { Input, Button } from 'reactstrap';
 
 function App() {
   const [spells, setSpells] = React.useState([]);
-  const [newSpellName, setNewSpellName] = React.useState();
+  const [username, setUsername] = React.useState();
 
   React.useEffect(() => {
     const fetchData = async () => {
       const db = firebase.firestore();
-      const data = await db.collection("spells").get();
+      const data = await db.collection("users").get();
       setSpells(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
     };
     fetchData();
@@ -18,16 +19,16 @@ function App() {
 
   const onCreate = () => {
     const db = firebase.firestore();
-    db.collection("spells").add({ name: newSpellName });
+    db.collection("users").add({ name: username });
   };
 
   return (
     <ul>
-      <input
-        value={newSpellName}
-        onChange={e => setNewSpellName(e.target.value)}
+      <Input
+        value={username}
+        onChange={e => setUsername(e.target.value)}
       />
-      <button onClick={onCreate}>Creat</button>
+      <Button onClick={onCreate}>Create</Button>
       {spells.map(spell => (
         <li key={spell.name}>
           <SpellInput spell={spell} />
--- src/SpellInput.js
+++ src/SpellInput.js
@@ -1,29 +1,31 @@
 import React from "react";
 import firebase from './firebase'
+import { Input, Button } from 'reactstrap';
+
 
 export const SpellInput = ({ spell }) => {
   const [name, setName] = React.useState(spell.name);
 
   const onUpdate = () => {
     const db = firebase.firestore()
-    db.collection('spells').doc(spell.id).set({...spell, name})
+    db.collection('users').doc(spell.id).set({...spell, name})
   }
 
   const onDelete = () => {
     const db = firebase.firestore()
-    db.collection('spells').doc(spell.id).delete()
+    db.collection('users').doc(spell.id).delete()
   }
 
   return (
     <>
-      <input
+      <Input
         value={name}
         onChange={e => {
           setName(e.target.value);
         }}
       />
-      <button onClick={onUpdate}>Updat</button>
-      <button onClick={onDelete}>Delet</button>
+      <Button onClick={onUpdate}>Update</Button>
+      <Button onClick={onDelete}>Delete</Button>
     </>
   );
 };
